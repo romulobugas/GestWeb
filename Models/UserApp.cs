8752f6f0f94ba9da5242d2ebe2f13f6173644da6
romulomauriz <romulo.mauriz@mobweb.net.br>
2024-02-16 21:51:45 -0300

    Adicionar arquivos de projeto.

---
+++ Models/UserApp.cs
@@ -0,0 +1,9 @@
+﻿namespace GestWeb.Models
+{
+    public class UserApp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; } // Já armazenado como hash
+    }
+}
---
+++ Services/AuthenticationService.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using GestWeb.Models;
+
+namespace GestWeb.Services
+{
+    public class AuthenticationService
+    {
+        private readonly HttpClient _http;
+        public string Token { get; private set; }
+        public bool IsLoading { get; private set; }
+        public bool IsError { get; private set; }
+
+        public AuthenticationService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<bool> LoginAsync(UserApp user)
+        {
+            IsLoading = true;
+            IsError = false;
+
+            try
+            {
+                var response = await _http.PostAsJsonAsync("http://localhost:7298/Auth/login", user);
+                if (response.IsSuccessStatusCode)
+                {
+                    var tokenResponse = await response.Content.ReadFromJsonAsync<TokenResponse>();
+                    if (tokenResponse != null)
+                    {
+                        Token = tokenResponse.Token;
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                IsError = true;
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+
+            return false;
+        }
+    }
+}
